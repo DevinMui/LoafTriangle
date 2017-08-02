cf6221fe47f788fe7cbd98463a52e2e204d6b6de
Jesse Liang <jesseliang@ymail.com>
2017-08-01 18:53:43 -0700

    added the 30 rssi thing no bluetooth tho

--- triangulation/scan.py
+++ triangulation/scan.py
@@ -9,6 +9,7 @@ import argparse
 import logging
 import statistics
 import atexit
+import datetime
 from multiprocessing import Process, Value, Manager
 from uuid import getnode as get_mac
 logger = logging.getLogger('scan.py')
@@ -103,7 +104,7 @@ def process_scan(time_window, own_node):
 				fingerprints[mac] = []
 			fingerprints[mac].append(float(rssi))
 		except:
-			print "pass"
+			# print "pass"
 			pass
 	logger.debug("..done")
 
@@ -138,6 +139,74 @@ def process_scan(time_window, own_node):
 		logger.debug(payload)
 		# return payload
 		return payload
+
+def calibrate(own_node, phone_mac):
+	rssis = []
+	while(len(rssis) < 30):
+		logger.debug("Reading files...")
+		output = ""
+		maxFileNumber = -1
+		fileNameToRead = ""
+		for filename in glob.glob("/tmp/tshark-temp*"):
+			fileNumber = int(filename.split("_")[1])
+			if fileNumber > maxFileNumber:
+				maxFileNumber = fileNumber
+				fileNameToRead = filename
+
+		logger.debug("Reading from %s" % fileNameToRead)
+		cmd = subprocess.Popen(("tshark -r "+fileNameToRead+" -T fields -e frame.time_epoch -e wlan.sa -e wlan.bssid -e radiotap.dbm_antsignal").split(
+		), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		output += cmd.stdout.read().decode('utf-8')
+
+		timestamp_threshold = float(time.time()) - float(time_window)
+		fingerprints = {}
+		relevant_lines = 0
+		for line in output.splitlines():
+			try:
+				timestamp, mac, mac2, power_levels = line.split("\t")
+				if mac == mac2 or float(timestamp) < timestamp_threshold or len(mac) == 0:
+					# print mac
+					# print float(timestamp) < timestamp_threshold
+					continue
+				
+				relevant_lines+=1
+				rssi = power_levels.split(',')[0]
+				if len(rssi) == 0:
+					continue
+
+				if mac not in fingerprints:
+					fingerprints[mac] = []
+				fingerprints[mac].append(float(rssi))
+				logger.debug(mac)
+			except:
+				pass
+		logger.debug("..done")
+
+		# print fingerprints
+
+		for mac in fingerprints:
+			if len(fingerprints[mac]) == 0:
+				continue
+			if str(mac) != phone_mac:
+				continue
+			ts = datetime.datetime.now()
+			rssis.append({
+				'rssi': int(statistics.median(fingerprints[mac])),
+				'timestamp': ts
+			})
+
+		logger.debug("Processed %d lines, found %d fingerprints in %d relevant lines" %
+					 (len(output.splitlines()), len(fingerprints2),relevant_lines))
+		time.sleep(float(1))
+	payload = {
+		"node": own_node,
+		"mac": own_mac,
+		"rssis": rssis
+	}
+	route = url +'/kalman'
+	r = requests.post(route, data=payload)
+	return r.json()
+
 
 def init(time_window, own_node, args):
 	other_nodes = []
