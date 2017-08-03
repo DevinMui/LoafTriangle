4eb95e90b7aeedb169683611367c4459e7f053cb
aahuangithub <aahuang@s.sfusd.edu>
2017-08-03 16:23:06 -0700

    map test

--- triangulation/map.py
+++ triangulation/map.py
@@ -2,31 +2,47 @@ from pylab import *
 import requests
 import json
 import time
-url = 'http://192.168.1.16:3000'
+url = 'http://192.168.1.8:3000'
 route = url+'/nodes'
 fake = url+'/fake'
 track = url+'/track'
 nodes = {}
 phones = {}
-
+oldPhones = {}
+ion()
+fig = figure()
+ax = fig.add_subplot(111)
 
 while 1:
+	# try:
+	r = requests.get(route)
+	mb = requests.get(track)
+	mbJSON = mb.json()
+	print mbJSON
+
+	if phones:
+		oldPhones = phones.copy()
+	phones.clear()
+
+	phones['x'] = mbJSON['android']['x']
+	phones['y'] = mbJSON['android']['y']
+	#ax = fig.add_subplot(111)
+	rJSON = r.json()
+	print rJSON
+	for node in rJSON:
+		nodes[node['x']] = node['y']
+	
 	try:
-		r = requests.get(route)
-		mb = requests.get(track)
-		mbJSON = mb.json()
-		for phone in mbJSON:
-			phones[phone['android']['x']] = phone['android']['y']
-		rJSON = r.json()
-		for node in rJSON:
-			nodes[node['x']] = node['y']
-		ax = pylab.subplot(max(int(k) for k in nodes.keys()))
-		ax.scatter(nodes.keys(), nodes.values(), '-ro')
-		ax.scatter(phones.keys(), phones.values(), '-ro', s=20)
-		ax.show()
-		time.sleep(1)
+		ax.scatter(oldPhones['x'], oldPhones['y'], s=10)
 	except:
-		print('fail, trying fake')
-		pls = requests.get(fake)
-		time.sleep(1)
+		print 'hi'
+	ax.scatter(nodes.keys(), nodes.values())
+	ax.scatter(phones['x'], phones['y'], s=20)
+	fig.show()
+	pause(0.001)
+	time.sleep(1)
+	# except:
+	# 	print('fail')
+	# 	#pls = requests.get(fake)
+	# 	time.sleep(1)
 
